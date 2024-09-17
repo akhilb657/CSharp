584d4ba1d90c155292056037282b57e55eeacd80
akhil <akhilboyanapalli657@gmail.com>
2024-09-17 16:07:35 -0500

    getting started

---
+++ Basics/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello dotnet developers");
+Console.WriteLine("Hello dotnet developers");
+Console.Write("First");
+Console.Write("Second");
+Console.WriteLine("Hello dotnet developers");
+Console.WriteLine("Third");
