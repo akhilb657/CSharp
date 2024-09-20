f69d6b880c9ff74812e8f8fd7749115ffb514564
akhil <akhilboyanapalli657@gmail.com>
2024-09-20 18:35:28 -0500

    namespaces

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,31 +1,8 @@
 ﻿using System;
+using Basics.Models;
 
 namespace Basics
 {
-    public class Computer
-    {
-        // private string _motherboard;
-        public string Motherboard {get; set;}
-        public int CPUCores {get; set;}
-        public bool HasWifi {get; set;}
-        public bool HasLTE {get; set;}
-        public DateTime ReleaseDate {get; set;}
-        public decimal Price {get; set;}
-        public string VideoCard {get; set;}
-
-        public Computer()
-        {
-            if(VideoCard == null)
-            {
-                VideoCard = "";
-            }
-
-            if(Motherboard == null)
-            {
-                Motherboard = "";
-            }
-        }
-    } 
     internal class Program
     {
         static void Main(string[] args)
