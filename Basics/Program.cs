17c89e40fc144fdb89436b5dd5504f514eb53576
akhil <akhilboyanapalli657@gmail.com>
2024-09-20 14:34:50 -0500

    Methods

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -6,47 +6,119 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            int myInt = 5;
-            int mySecondInt = 10;
+            int[] intsToCompress = new int[] {1, 2, 3, 4, 5, 6, 45, 11, 22, 31};
 
-            if(myInt < mySecondInt)
+            int totalValue = 0;
+
+            foreach (int intForCompression in intsToCompress)
             {
-                myInt += 10;
+                if(intForCompression > 40)
+                {
+                    totalValue += intForCompression;
+                }
             }
 
-            Console.WriteLine(myInt);
+            Console.WriteLine(totalValue);
 
-            string myCow = "Cows";
-            // string myCapitalizedCow = "cow";
 
-            // if (myCow == myCapitalizedCow)
-            // {
-            //     Console.WriteLine("Equal");
-            // }
+            DateTime startTime = DateTime.Now;
 
-            // else if(myCow == myCapitalizedCow.ToLower())
-            // {
-            //     Console.WriteLine("Equal without case sensitivity");
-            // }
-            // else{
-            //     Console.WriteLine("Not Equal");
-            // }
+            totalValue = intsToCompress[0] + intsToCompress[1] + intsToCompress[2] + intsToCompress[3] + intsToCompress[4] + intsToCompress[5] + intsToCompress[6] + intsToCompress[7] + intsToCompress[8] + intsToCompress[9];
 
-            switch (myCow)
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            Console.WriteLine(totalValue); // 129
+
+            totalValue = 0;
+
+            startTime = DateTime.Now;
+
+            for(int i = 0; i<intsToCompress.Length; i++)
             {
-                case "cow":
-                    Console.WriteLine("Lowercase");
-                    break;
-                
-                case "Cow":
-                    Console.WriteLine("Capitalized");
-                    break;
-                
-                default:
-                    Console.WriteLine("Default Case"); 
-                    break;
+                totalValue += intsToCompress[i];
+            }
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            Console.WriteLine(totalValue);
+
+           
+
+            startTime = DateTime.Now;
+
+            
+
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            Console.WriteLine(totalValue);
+
+            totalValue = 0;
+
+            startTime = DateTime.Now;
+
+            int index = 0;
+
+            while(index  < intsToCompress.Length)
+            {
+                totalValue += intsToCompress[index];
+                index++;
             }
 
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            Console.WriteLine(totalValue);
+
+            totalValue = 0;
+
+             
+
+            index = 0;
+
+            do
+            {
+                totalValue += intsToCompress[index];
+
+                index++;
+            } 
+            while(index  < intsToCompress.Length);
+
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            Console.WriteLine(totalValue);
+
+            startTime = DateTime.Now;
+
+            totalValue = intsToCompress.Sum();
+
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            Console.WriteLine(totalValue);
+
+            startTime = DateTime.Now;
+
+            totalValue = GetSum(intsToCompress);
+
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+
+            Console.WriteLine(totalValue);
+
+            int[] nums = new int[] { 10, 20, 30, 40 , 50 , 60};
+
+            totalValue = GetSum(nums);
+
+            Console.WriteLine(totalValue);
+
+
+        }
+
+        static private int GetSum(int[] intsToCompress)
+        {
+            // int[] intsToCompress = new int[] {1, 2, 3, 4, 5, 6, 45, 11, 22, 31};
+            int totalValue = 0;
+            foreach (int intForCompression in intsToCompress)
+            {  
+                totalValue += intForCompression; 
+            }
+            return totalValue;
         }
     }
 }
--- Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
+++ Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b0e5de7cb0d3cd622545bc6b915964f708b0ca6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5addc6b798eb43b0e3605fa30fe4fd825c7ff08d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
