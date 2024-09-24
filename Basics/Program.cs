bf1e8659c6cb61834c342b55fc7bf7d88557c898
akhil <akhilboyanapalli657@gmail.com>
2024-09-24 15:07:08 -0500

    writing to a file and reading from file

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -14,20 +14,6 @@ namespace Basics
         static void Main(string[] args)
         {
 
-            IConfiguration config = new ConfigurationBuilder()
-                .AddJsonFile("appSettings.json")
-                .Build();
-
-            DataContextDapper dapper = new DataContextDapper(config);
-
-            DataContextEF entityFramework = new DataContextEF(config);
-
-            string sqlCommad = "SELECT GETDATE()";
-
-            DateTime rightNow = dapper.LoadDataSingle<DateTime>(sqlCommad);
-
-            // Console.WriteLine(rightNow);
-
             Computer myComputer = new Computer() {
                 Motherboard = "Z690",
                 HasWifi = true,
@@ -37,8 +23,7 @@ namespace Basics
                 VideoCard = "RTX 2060"
             };
 
-            entityFramework.Add(myComputer);
-            entityFramework.SaveChanges();
+         
 
             string sql = @"INSERT INTO TutorialAppSchema.Computer (
                 Motherboard,
@@ -55,70 +40,18 @@ namespace Basics
                     + "','" + myComputer.VideoCard 
             + "')";
 
-            // Console.WriteLine(sql);
+            File.WriteAllText("log.txt", "\n" + sql + "\n");
 
-            bool result = dapper.ExecuteSql(sql);
+            using StreamWriter openFile = new("log.txt", append: true);
 
-            // Console.WriteLine(result);
+            openFile.WriteLine("\n" + sql + "\n");
 
-            string sqlSelect = @"SELECT 
-                Computer.ComputerId,
-                Computer.Motherboard,
-                Computer.HasWifi,
-                Computer.HasLTE,
-                Computer.ReleaseDate,
-                Computer.Price,
-                Computer.VideoCard
-             FROM TutorialAppSchema.Computer";
+            openFile.Close();
 
-            IEnumerable<Computer> computers = dapper.LoadData<Computer>(sqlSelect);
+            string fileText = File.ReadAllText("log.txt");
 
-            // IEnumerable<Computer>? computersEF = entityFramework.Computer?.ToList<Computer>();
-
-            Console.WriteLine("'ComputerId', 'Motherboard', 'HasWifi', 'HasLTE', 'ReleaseDate', 'Price', 'VideoCard'");
-
-            foreach(Computer singleComputer in computers)
-            {
-                Console.WriteLine("'" + singleComputer.ComputerId 
-                    + "','" + singleComputer.Motherboard
-                    + "','" + singleComputer.HasWifi
-                    + "','" + singleComputer.HasLTE
-                    + "','" + singleComputer.ReleaseDate.ToString("yyyy-MM-dd")
-                    + "','" + singleComputer.Price
-                    + "','" + singleComputer.VideoCard 
-                    +"'");
-            }
-
-            IEnumerable<Computer>? computersEF = entityFramework.Computer?.ToList<Computer>();
-
+            Console.WriteLine(fileText);
             
-            if(computersEF != null)
-            {
-                Console.WriteLine("'ComputerId', 'Motherboard', 'HasWifi', 'HasLTE', 'ReleaseDate', 'Price', 'VideoCard'");
-
-                foreach(Computer singleComputer in computersEF)
-                {
-                    Console.WriteLine("'" + singleComputer.ComputerId 
-                        + "','" + singleComputer.Motherboard
-                        + "','" + singleComputer.HasWifi
-                        + "','" + singleComputer.HasLTE
-                        + "','" + singleComputer.ReleaseDate.ToString("yyyy-MM-dd")
-                        + "','" + singleComputer.Price
-                        + "','" + singleComputer.VideoCard 
-                        +"'");
-                }
-            }
-
-            // myComputer.HasWifi = false;
-
-            // Console.WriteLine(myComputer.Motherboard);
-            // Console.WriteLine(myComputer.HasLTE);
-            // Console.WriteLine(myComputer.HasWifi);
-            // Console.WriteLine(myComputer.ReleaseDate);
-            // Console.WriteLine(myComputer.VideoCard);
-            // Console.WriteLine(myComputer.Price);
-
-            // myComputer.ReleaseDate.ToString("yyyy-MM-dd");
         }
     }
 }
